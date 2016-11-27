1df27fa593099acc42dbacf9153596d21b9454e4
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-26 23:13:06 -0800

    Reformat code

--- main.go
+++ main.go
@@ -1,40 +1,39 @@
-package main
+package feedupdater
 
 import (
-    "google.golang.org/appengine/taskqueue"
-    "google.golang.org/appengine"
-    "net/http"
-    "net/url"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/taskqueue"
+	"net/http"
+	"net/url"
 )
 
 type Feed struct {
-    FeedId string
-    FeedUrl string
+	FeedId  string
+	FeedUrl string
 }
 
 func init() {
-    feeds := []Feed{
-        Feed{"dummy1", "http://rss.slashdot.org/Slashdot/slashdotMainatom"},
-    }
+	feeds := []Feed{
+		{"dummy1", "http://rss.slashdot.org/Slashdot/slashdotMainatom"},
+	}
 
-    http.HandleFunc("/updates", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPut {
-            http.Error(w, "Not allowed", http.StatusMethodNotAllowed)
-            return
-        }
+	http.HandleFunc("/updates", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			http.Error(w, "Not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-        ctx := appengine.NewContext(r)
-        for _, feed := range feeds {
-            t := taskqueue.NewPOSTTask("/handle", url.Values{
-                "feed_id": {feed.FeedId},
-                "feed_url": {feed.FeedUrl},
-            })
+		ctx := appengine.NewContext(r)
+		for _, feed := range feeds {
+			t := taskqueue.NewPOSTTask("/handle", url.Values{
+				"feed_id":  {feed.FeedId},
+				"feed_url": {feed.FeedUrl},
+			})
 
-            if  _, err := taskqueue.Add(ctx, t, "crawlerqueue"); err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-            }
-        }
-    })
+			if _, err := taskqueue.Add(ctx, t, "CrawlerQueue"); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	})
 
 }
-
