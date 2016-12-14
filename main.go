94c755cf43bdf7edf02d75b2c1ea073adb529eae
Mateusz Haligowski <mhaligowski@gmail.com>
2016-12-13 22:09:19 -0800

    Refactor the input and output to use the new Paperboy feed format

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/taskqueue"
+	"github.com/mhaligowski/paperboy-feeds"
+	"encoding/json"
 )
 
-type Feed struct {
-	FeedId  string
-	FeedUrl string
-}
-
 func init() {
-	feeds := []Feed{
-		{"dummy1", "http://rss.slashdot.org/Slashdot/slashdotMainatom"},
+	feeds := []feeds.Feed{
+		{"ff0b434e9374265b95316f6e5d09193eb4f81bbc760cc508308d6ee4da5af339",
+			"Slashdot",
+			"http://rss.slashdot.org/Slashdot/slashdotMainatom"},
 	}
 
 	http.HandleFunc("/updates", func(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +24,16 @@ func init() {
 		}
 
 		ctx := appengine.NewContext(r)
+
 		for _, feed := range feeds {
-			t := taskqueue.NewPOSTTask("/handle", url.Values{
-				"feed_id":  {feed.FeedId},
-				"feed_url": {feed.FeedUrl},
-			})
+			t := taskqueue.NewPOSTTask("/handle", url.Values{})
+			v, err := json.Marshal(feed)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			t.Payload = v
+			t.Header.Set("Content-Type", "application/json")
+
 
 			if _, err := taskqueue.Add(ctx, t, "CrawlerQueue"); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
